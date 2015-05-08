ac0783ad383292e2fba8577cac6c8fe47dcd50c5
henrywlu <henrywlu@yahoo.com>
2015-05-08 13:29:54 -0700

    Initial Commit

---
+++ VideoSnakeT01/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  VideoSnakeT01
+//
+//  Created by Henry W. Lu on 5/8/15.
+//  Copyright (c) 2015 Henry W. Lu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ VideoSnakeT01/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VideoSnakeT01
+//
+//  Created by Henry W. Lu on 5/8/15.
+//  Copyright (c) 2015 Henry W. Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
